8e0fdae3982a7d9e779b0991bbd6988de8936ef1
Calvao <hgohugo0@gmail.com>
2026-01-26 11:22:08 -0300

    Inicio projeto python

---
+++ codigos/dia_01/01_ola_mundo.py
@@ -0,0 +1,8 @@
+# %%
+
+print("Vai Corinthians!!!!!!!!!!!!!!!!!!!!!!!")
+
+# %%
+
+print("Legal")
+# %%
---
+++ codigos/dia_01/02_calc.py
@@ -0,0 +1,24 @@
+# %%
+
+# SOMA
+print("1 + 1=",1+1)
+
+# SUBTRAÇÂO
+print("10 - 5=",10-5)
+# %%
+# MULTIPLICACAO
+print("10 X 5=",10*5)
+
+# DIVISAO
+print("10 / 5=", 10/5)
+
+# DIVISAO COM INTEIRA PARTE
+print("10 // 3 =", 10 // 3)
+
+# RESTO DA DIVISAO
+print("10 / 4 =", 10 % 4)
+
+# POTENCIA
+print("3 ** 3 =", 3 ** 4)
+# %%
+
