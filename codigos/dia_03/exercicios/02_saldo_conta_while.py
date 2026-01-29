712bb8ae2407ae832661fab8bb581d412f5c0bd6
Calvao <hgohugo0@gmail.com>
2026-01-28 21:33:26 -0300

    Alteração de condição para parar e realizar a soma do saldo

--- codigos/dia_03/exercicios/02_saldo_conta_while.py
+++ codigos/dia_03/exercicios/02_saldo_conta_while.py
@@ -1,10 +1,12 @@
 saldo_total = 0
+saldo=0
+while saldo!= "":    
+    saldo = input("""
+                  (APERTE ENTER SEM ADICIONAR VALOR PARA SOMAR O SALDO)\n
+                  Entre com o saldo:
+                  """)
 
-while True:
-    saldo = input("Entre com o saldo: ")
-    if saldo == "":
-        break
-
-    saldo_total += float(saldo)
+    if saldo != "":
+            saldo_total += float(saldo)
 
 print("Saldo total: R$",saldo_total)
