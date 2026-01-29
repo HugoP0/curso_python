41e0a8a9200ed8b886820ee5a0ea0393446aa31c
Calvao <hgohugo0@gmail.com>
2026-01-28 21:20:25 -0300

    Nova forma de realizar a tabuada, com inputs do usuario

--- codigos/dia_03/01_tabuada_for.py
+++ codigos/dia_03/01_tabuada_for.py
@@ -1,7 +1,7 @@
 # %%
-numero = 2
-max_numero = 100
+numero = input("Digite o número que deseja a tabuada: ")
+max_numero= int((input)("Digite quantas multiplicações você deseja:"))
 
-for i in range(1, max_numero+1):
-    print(numero, "X", i, "=", numero * i)
+for i in range(1,max_numero+1):
+    print(numero, "X", i, "=", int(numero) * i)
 # %%
--- codigos/dia_03/01_tabuada_while.py
+++ codigos/dia_03/01_tabuada_while.py
@@ -1,11 +1,11 @@
 # %%
 count = 0
-numero = 2
-while count<=10: 
+numero = input("Digite o número que deseja a tabuada: ")
+max_numero= int((input)("Digite quantas multiplicações você deseja:"))
+
+while count<=max_numero: 
     if count >= 1:
-        print(numero, "X", count, "=", numero * count)
+        print(numero, "X", count, "=", int(numero) * count)
     
     count += 1
-
-print("Cabou")
 # %%
